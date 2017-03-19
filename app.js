f16a8e2b00b4f697e2b06763b421482d12f3dc57
souleex <souleex@gmail.com>
2017-03-19 16:23:16 -0700

    Finished section 1 - "What is REST"

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send("welcome to my api");
+});
+
+app.listen(port, function(){
+    console.log("Gulp is running my app on Port: " + port);
+});
