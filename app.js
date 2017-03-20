c990b3007a8f6f1656db742158f409624d90ace5
souleex <souleex@gmail.com>
2017-03-19 17:48:34 -0700

    finsished implementation of bookModeljs

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var express = require("express");
+    mongoose = require("mongoose");
+    
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/books').get(function(req,res){
+    book.find(function(err,books){
+        if (err) {
+            res.status(500).send(err);
+        }else{
+            res.json(books);
+        }
+    });
+});
+
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res){
     res.send("welcome to my api");
